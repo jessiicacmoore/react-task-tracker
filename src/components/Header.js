bda1ba902448642db77ee9a493f687e5599a9295
Jessica Moore <jessica.cmoore1@gmail.com>
2021-01-26 20:54:55 -0500

    commit

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title, showAddTask, onAdd }) => {
+  const onClick = () => {
+    console.log('click');
+  }
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button color={showAddTask ? 'red' : 'green'} text={showAddTask ? 'Close' : 'Add'} onClick={onAdd} />
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker'
+}
+
+Header.propTypes = {
+  title: PropTypes.string
+}
+
+export default Header
